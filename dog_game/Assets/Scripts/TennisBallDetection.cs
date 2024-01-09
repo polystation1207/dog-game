bc4899d073ab1ca668c1b1b8def14f3fbb2b1f7b
polystation1207 <25.omar.fierro@ksd.org>
2024-01-09 10:00:10 -0800

    fixed colliders and added tennis ball counter

--- dog_game/Assets/Scripts/TennisBallDetection.cs
+++ dog_game/Assets/Scripts/TennisBallDetection.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TennisBallDetection : MonoBehaviour
 {
+    [SerializeField] float ballCount;
+    [SerializeField] TextMeshProUGUI myText;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Tennis Ball")
         {
+            ballCount += 1;
+            myText.text = "tennis balls: " + ballCount;
             Destroy(other.gameObject);
         }
 
